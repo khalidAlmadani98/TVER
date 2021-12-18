b355272414dc3575f0dd4dd43ca76669bc1d12c0
khalidAlmadani98 <78568064+khalidAlmadani98@users.noreply.github.com>
2021-12-18 22:56:16 +0300

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,26 @@
+
+/**
+ * Java Program - Average of Numbers
+ */
+ 
+public class Average {
+ 
+    public static void main(String[] args) {
+        //numbers
+        int[] nums = {1, 2, 3, 4, 5, 6};
+         
+        float sum = 0;
+         
+        //compute sum
+        int i=0;
+        while(i < nums.length) {
+            sum += nums[i];
+            i++;
+        }
+         
+        //compute average
+        float average = (sum / nums.length); 
+         
+        System.out.println("Average : "+average);
+    }
+}
